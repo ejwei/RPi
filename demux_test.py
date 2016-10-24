5a40b1c6dc19093200a3698a0fc5b02687f1d087
Eric Wei <ericwei@berekeley.edu>
2016-10-24 16:17:37 -0700

    Added Serial to demux_test

--- demux_test.py
+++ demux_test.py
@@ -1,5 +1,9 @@
 import RPi._GPIO as GPIO
 import time
+import serial
+
+# Setup Serial
+port = serial.Serial("/dev/ttyAMA0", baudrate = 115200, timeout = 0)
 
 #set up GPIO using BCM numbering
 GPIO.setmode(GPIO.BCM)
@@ -12,6 +16,7 @@ GPIO.setmode(GPIO.BCM)
 # A1 - GPIO 27
 # A2 - GPIO 22
 
+# Setup the Outputs
 A0 = 17
 A1 = 27
 A2 = 22
@@ -20,18 +25,38 @@ GPIO.setup(A0, GPIO.OUT)
 GPIO.setup(A1, GPIO.OUT)
 GPIO.setup(A2, GPIO.OUT)
 
+
+
 GPIO.output(A0, False)
 GPIO.output(A1, False)
 GPIO.output(A2, False)
+
+while True
+	if(serial.inWaiting() > 0):
+		rcv = port.readline()
+		print(rcv)
+		break
 
 time.sleep(1)
 
 GPIO.output(A0, True)
 GPIO.output(A1, False)
 GPIO.output(A2, False)
+
+while True
+	if(serial.inWaiting() > 0):
+		rcv = port.readline()
+		print(rcv)
+		break
 
 time.sleep(1)
 
 GPIO.output(A0, True)
 GPIO.output(A1, True)
 GPIO.output(A2, False)
+
+while True
+	if(serial.inWaiting() > 0):
+		rcv = port.readline()
+		print(rcv)
+		break
--- serialtest.py
+++ serialtest.py
@@ -1,4 +1,4 @@
-import serial from pySerial
+import serial
 port = serial.Serial("/dev/ttyAMA0", baudrate = 115200, timeout = 3.0)
 
 while True:
