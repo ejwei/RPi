a2bc76b3f32be647df41e7a11f4b3740d5becc1c
Eric Wei <ericwei@berekeley.edu>
2016-10-25 10:41:27 -0700

    reduce time

--- demux_test.py
+++ demux_test.py
@@ -49,25 +49,25 @@ def testThread():
 	GPIO.setup(A2, GPIO.OUT)
 	
 	thread2.start()
-	time.sleep(5)
+	time.sleep(0.1)
 	
 	GPIO.output(A0, False)
 	GPIO.output(A1, False)
 	GPIO.output(A2, False)
 
-	time.sleep(5)
+	time.sleep(0.1)
 
 	GPIO.output(A0, True)
 	GPIO.output(A1, False)
 	GPIO.output(A2, False)
 
-	time.sleep(5)
+	time.sleep(0.1)
 
 	GPIO.output(A0, True)
 	GPIO.output(A1, True)
 	GPIO.output(A2, False)
 
-	time.sleep(5)
+	time.sleep(0.1)
 
 	print("Test Completed!")
 	testComplete = 1
