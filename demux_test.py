6d7f8009555b49e64e22d8b960c9e761dbd52565
Eric Wei <ericwei@berekeley.edu>
2016-10-24 17:40:27 -0700

    Increase Time

--- demux_test.py
+++ demux_test.py
@@ -36,20 +36,20 @@ def testThread():
 	GPIO.setup(A2, GPIO.OUT)
 	
 	thread2.start()
-	time.sleep(1)
+	time.sleep(60)
 
 
 	GPIO.output(A0, False)
 	GPIO.output(A1, False)
 	GPIO.output(A2, False)
 
-	time.sleep(1)
+	time.sleep(60)
 
 	GPIO.output(A0, True)
 	GPIO.output(A1, False)
 	GPIO.output(A2, False)
 
-	time.sleep(1)
+	time.sleep(60)
 
 	GPIO.output(A0, True)
 	GPIO.output(A1, True)
