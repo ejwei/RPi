7a16d075067417e1ea089adc71e79f38299981a5
Eric Wei <ericwei@berekeley.edu>
2016-10-24 17:24:25 -0700

    Added multiple threads

--- demux_test.py
+++ demux_test.py
@@ -1,6 +1,7 @@
 import RPi._GPIO as GPIO
 import time
 import serial
+import threading
 
 # Setup Serial
 port = serial.Serial("/dev/ttyAMA0", baudrate = 115200)
@@ -21,50 +22,41 @@ A0 = 17
 A1 = 27
 A2 = 22
 
-GPIO.setup(A0, GPIO.OUT)
-GPIO.setup(A1, GPIO.OUT)
-GPIO.setup(A2, GPIO.OUT)
-
-GPIO.output(A0, True)
-GPIO.output(A1, True)
-GPIO.output(A2, False)
-
-time.sleep(1)
-
-GPIO.output(A0, False)
-GPIO.output(A1, False)
-GPIO.output(A2, False)
-
-while True:
-	if(port.inWaiting() > 0):
-		rcv = port.readline()
+def printThread():
+	while True:
+		rcv = port.readLine()
 		print(rcv)
-		break
 
-time.sleep(1)
+def testThread():
+	GPIO.setup(A0, GPIO.OUT)
+	GPIO.setup(A1, GPIO.OUT)
+	GPIO.setup(A2, GPIO.OUT)
+	
+	printThread()
+	time.sleep(1)
 
-GPIO.output(A0, True)
-GPIO.output(A1, False)
-GPIO.output(A2, False)
 
-while True:
-	if(port.inWaiting() > 0):
-		rcv = port.readline()
-		print(rcv)
-		break
+	GPIO.output(A0, False)
+	GPIO.output(A1, False)
+	GPIO.output(A2, False)
 
-time.sleep(1)
+	printThread()
+	time.sleep(1)
 
-GPIO.output(A0, True)
-GPIO.output(A1, True)
-GPIO.output(A2, False)
+	GPIO.output(A0, True)
+	GPIO.output(A1, False)
+	GPIO.output(A2, False)
 
-while True:
-	if(port.inWaiting() > 0):
-		rcv = port.readline()
-		print(rcv)
-		break
+	printThread()
+	time.sleep(1)
 
-port.close()
+	GPIO.output(A0, True)
+	GPIO.output(A1, True)
+	GPIO.output(A2, False)
 
-print("Test Completed!")
+	port.close()
+
+	print("Test Completed!")
+
+thread = threading.Thread(target=testThread)
+thread.start()
